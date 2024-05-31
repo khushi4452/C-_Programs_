e6badb31c88b95973d40059dd7b5460490090288
KHUSHI 444 <147485713+khushi4452@users.noreply.github.com>
2024-05-31 17:39:15 +0530

    Add files via upload

---
+++ stl.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+int main(){
+
+    vector<int>v; //create vector;
+
+    //size and capacity
+    cout<<"Size of v: "<<v.size()<<endl;
+    cout<<"capacity of v: "<<v.capacity()<<endl;
+
+
+
+}
